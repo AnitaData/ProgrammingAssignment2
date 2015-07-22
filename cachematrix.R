bde729c9a24de4f9462bcf52c8718db3aa038d54
anitacheung <cheunganita@gmail.com>
2015-07-22 09:36:13 -0400

    updated assignment to clean up notes section

--- cachematrix.R
+++ cachematrix.R
@@ -4,9 +4,6 @@
 # set the value of inverse of the matrix
 # get the value of inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 makeCacheMatrix <- function(x = matrix) {
         inv <- NULL
@@ -22,7 +19,12 @@ makeCacheMatrix <- function(x = matrix) {
              getmean = getmean)
 }
 
-## The following function calculates the inverse cached created with the above function. However, it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the matrix sets in the cache via the setinv function.
+# The following function calculates the inverse cached 
+# created with the above function. However, it first checks 
+# to see if the inverse has already been calculated. If so, it 
+# gets the inverse from the cache and skips the computation. 
+# Otherwise, it calculates the inverse of the matrix sets in the 
+# cache via the setinv function.
 
 
 cacheSolve <- function(x, ...) {
